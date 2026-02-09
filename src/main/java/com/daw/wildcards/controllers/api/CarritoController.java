77ae23bc5527c334dd377d380e9efb32f5553714
samdres27 <samdres27@gmial.com>
2026-02-09 13:30:59 +0100

    Cambio controladora carrito api

--- src/main/java/com/daw/wildcards/controllers/api/CarritoController.java
+++ src/main/java/com/daw/wildcards/controllers/api/CarritoController.java
@@ -1,5 +1,10 @@
 package com.daw.wildcards.controllers.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class CarritoController {
 
 }
