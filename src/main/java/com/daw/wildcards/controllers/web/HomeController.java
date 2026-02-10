181656f20c1944198d19c3d07dc7b9c693dd882e
zx25student347-commits <zx25student347@colegiovalledelmiro.es>
2026-02-10 08:23:05 +0100

    Arreglo para conectarse a la BD

--- src/main/java/com/daw/wildcards/controllers/web/HomeController.java
+++ src/main/java/com/daw/wildcards/controllers/web/HomeController.java
@@ -1,5 +1,17 @@
 package com.daw.wildcards.controllers.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String inicio() {
+        return "index";
+    }
+    
 
 }
