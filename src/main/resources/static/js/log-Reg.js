426b7944f614629c18b29b4b04783bc8bd429bc4
zx25student347-commits <zx25student347@colegiovalledelmiro.es>
2026-02-19 09:56:51 +0100

    Arreglo del login

--- src/main/resources/static/js/log-Reg.js
+++ src/main/resources/static/js/log-Reg.js
@@ -23,31 +23,4 @@ document.getElementById("registerForm").addEventListener("submit", async functio
     }
 });
 
-document.getElementById("loginForm").addEventListener("submit", async function(e) {
-    e.preventDefault();
 
-    const data = {
-        username: document.getElementById("email").value,
-        password: document.getElementById("password").value
-    };
-
-    const response = await fetch("/auth/login", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(data)
-    });
-
-    if (!response.ok) {
-        alert("Usuario o contraseña incorrectos");
-        return;
-    }
-
-    const result = await response.json();
-    
-    if (result.token) {
-        localStorage.setItem("token", result.token);
-        window.location.href = "/"; // URL limpia a la raíz
-    }
-});
